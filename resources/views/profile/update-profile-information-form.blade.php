c7108ade9be775009ad7b25404a3d5536a8491ab
daugaard47 <daugaard47@gmail.com>
2024-01-29 23:29:39 -0500

    Merged the Account-Managment branch to main

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -100,6 +100,7 @@
         <button type="submit" wire:loading.attr="disabled" wire:target="photo" class="ms-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
             {{ __('Save') }}
         </button>
+        <!-- Conflict with Mary UI / Jetstream button -->
 {{--        <x-button wire:loading.attr="disabled" wire:target="photo">--}}
 {{--            {{ __('Save') }}--}}
 {{--        </x-button>--}}
