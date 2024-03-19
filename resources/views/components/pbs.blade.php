c8c32ad4a0ba34e87ee2fe82a038a080c459db3b
Wish Born <wish@impact.do>
2024-03-19 17:35:09 -0500

    added link to impact-hub icon

--- resources/views/components/pbs.blade.php
+++ resources/views/components/pbs.blade.php
@@ -1,4 +1,4 @@
 <div style="position: fixed; bottom: 10px; right: 10px;">
-        <img src="/imgs/impacthub_logo-100x100.png" alt="Powered by ImpactHub" class="max-w-12 mask mask-squircle" />
+        <a href="/impact-hub" wire:navigate><img src="/imgs/impacthub_logo-100x100.png" alt="Powered by ImpactHub" class="max-w-12 mask mask-squircle" /></a>
     </div>
 
